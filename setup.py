389c151498f7fef372a24900a48421dc2cbf0632
Subhashsingh Rajpurohit <39688957+Subhash37@users.noreply.github.com>
2025-05-20 11:07:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="Canoe_PY",
-    version="0.1.0",
+    version="0.1.1",
     author="Subhashsingh Rajpurohit",
     author_email="rajpurohits001@example.com",
     description="A custom library for interacting with Vector CANoe",
